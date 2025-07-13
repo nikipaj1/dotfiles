9205514e124e3da9529947586b6241c1c0db149c
nikipaj1 <nikita.pajanok@gmail.com>
2025-07-13 20:43:20 +0300

    reactjs etc. support

--- config/nvim/lua/plugins/typescript.lua
+++ config/nvim/lua/plugins/typescript.lua
@@ -45,6 +45,94 @@ return {
     },
   },
 
+  -- Tailwind CSS IntelliSense
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ft = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    opts = {
+      document_color = {
+        enabled = true,
+        kind = "inline",
+        inline_symbol = "󰝤 ",
+        debounce = 200,
+      },
+      conceal = {
+        enabled = false,
+        min_length = nil,
+        symbol = "…",
+        highlight = {
+          fg = "#808080",
+        },
+      },
+    },
+  },
+
+  -- Auto-closing JSX tags
+  {
+    "windwp/nvim-ts-autotag",
+    ft = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+    config = true,
+  },
+
+  -- React-specific snippets
+  {
+    "dsznajder/vscode-es7-javascript-react-snippets",
+    build = "yarn install --frozen-lockfile && yarn compile",
+    ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  },
+
+  -- Harpoon for quick file navigation
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ha", function() require("harpoon"):list():add() end, desc = "Add file to harpoon" },
+      { "<leader>hh", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Toggle harpoon menu" },
+      { "<leader>h1", function() require("harpoon"):list():select(1) end, desc = "Harpoon file 1" },
+      { "<leader>h2", function() require("harpoon"):list():select(2) end, desc = "Harpoon file 2" },
+      { "<leader>h3", function() require("harpoon"):list():select(3) end, desc = "Harpoon file 3" },
+      { "<leader>h4", function() require("harpoon"):list():select(4) end, desc = "Harpoon file 4" },
+    },
+    config = function()
+      require("harpoon"):setup()
+    end,
+  },
+
+  -- Testing with Neotest
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/neotest-jest",
+      "marilari88/neotest-vitest",
+    },
+    keys = {
+      { "<leader>tr", function() require("neotest").run.run() end, desc = "Run nearest test" },
+      { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run file tests" },
+      { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle test summary" },
+      { "<leader>to", function() require("neotest").output.open({ enter = true }) end, desc = "Open test output" },
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-jest")({
+            jestCommand = "npm test --",
+            jestConfigFile = "jest.config.js",
+            env = { CI = true },
+            cwd = function(path)
+              return vim.fn.getcwd()
+            end,
+          }),
+          require("neotest-vitest"),
+        },
+      })
+    end,
+  },
+
   -- TypeScript utilities
   {
     "pmizio/typescript-tools.nvim",
@@ -66,7 +154,7 @@ return {
         code_lens = "off",
         disable_member_code_lens = true,
         jsx_close_tag = {
-          enable = false,
+          enable = true,
           filetypes = { "javascriptreact", "typescriptreact" },
         },
       },
