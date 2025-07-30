960ff07030986197717d33f5432a5f8db6ca3faa
nikipaj1 <nikita.pajanok@gmail.com>
2025-07-30 10:30:52 +0300

    add git conflict resolution

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -74,6 +74,38 @@ return {
     },
   },
 
+  -- Git conflict resolution
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    config = function()
+      require("git-conflict").setup({
+        default_mappings = {
+          ours = 'co',
+          theirs = 'ct',
+          none = 'c0',
+          both = 'cb',
+          next = ']x',
+          prev = '[x',
+        },
+        default_commands = true, -- disable commands created by this plugin
+        disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+        list_opener = 'copen', -- command or function to open the conflicts list
+        highlights = { -- They must have background color, otherwise the default color will be used
+          incoming = 'DiffAdd',
+          current = 'DiffText',
+        }
+      })
+      
+      -- Additional keymaps for git conflicts
+      vim.keymap.set('n', '<leader>gc', '<cmd>GitConflictListQf<cr>', { desc = 'List git conflicts' })
+      vim.keymap.set('n', '<leader>gco', '<cmd>GitConflictChooseOurs<cr>', { desc = 'Choose our changes' })
+      vim.keymap.set('n', '<leader>gct', '<cmd>GitConflictChooseTheirs<cr>', { desc = 'Choose their changes' })
+      vim.keymap.set('n', '<leader>gcb', '<cmd>GitConflictChooseBoth<cr>', { desc = 'Choose both changes' })
+      vim.keymap.set('n', '<leader>gcn', '<cmd>GitConflictChooseNone<cr>', { desc = 'Choose none of the changes' })
+    end,
+  },
+
   -- Better commenting
   {
     "numToStr/Comment.nvim",
